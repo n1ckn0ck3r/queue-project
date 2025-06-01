856a90164c90de271f5dc53b4bad111da3ef7380
n1ckn0ck3r <bondarenko.kolya.a@gmail.com>
2025-06-01 20:44:32 +0300

    networking

--- src/main/java/com/example/queue/config/SecurityConfig.java
+++ src/main/java/com/example/queue/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://26.61.69.90:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
